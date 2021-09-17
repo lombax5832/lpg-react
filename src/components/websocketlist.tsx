21585a2fc24b044059e65ec484fb8da6d5cad3c9
Alexei Snytkine <ipodalexei@gmail.com>
2021-09-16 22:44:58 -0400

    update in chunks

--- src/components/websocketlist.tsx
+++ src/components/websocketlist.tsx
@@ -4,7 +4,22 @@ import WebSocketContext from "../context/websocketcontext";
 import { ServerMessage } from "../interfaces/servermessage";
 import Chart from "./chart";
 
-class WebSocketList extends Component<{ ws: WebSocket | null }, { messages: ServerMessage[] }>{
+interface Data {
+    PT_HE: number[]
+    PT_Purge: number[]
+    PT_Pneu: number[]
+    PT_FUEL_PV: number[]
+    PT_LOX_PV: number[]
+    PT_FUEL_INJ: number[]
+    PT_CHAM: number[]
+    TC_FUEL_PV: number[]
+    TC_LOX_PV: number[]
+    TC_LOX_Valve_Main: number[]
+    RC_LOX_Level: number[]
+    FT_Thrust: number[]
+}
+
+class WebSocketList extends Component<{ ws: WebSocket | null }, { data: Data }>{
 
     static contextType = WebSocketContext;
 
@@ -12,55 +27,64 @@ class WebSocketList extends Component<{ ws: WebSocket | null }, { messages: Serv
         super(props);
 
         this.state = {
-            messages: [],
+            data: {
+                PT_HE: [],
+                PT_Purge: [],
+                PT_Pneu: [],
+                PT_FUEL_PV: [],
+                PT_LOX_PV: [],
+                PT_FUEL_INJ: [],
+                PT_CHAM: [],
+                TC_FUEL_PV: [],
+                TC_LOX_PV: [],
+                TC_LOX_Valve_Main: [],
+                RC_LOX_Level: [],
+                FT_Thrust: [],
+            }
         }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextState.data.PT_HE.length % 20 !== 0)
+            return false
+        return true
     }
 
     componentDidUpdate() {
         if (this.props.ws && !this.props.ws.onmessage) {
             this.props.ws.onmessage = (event: MessageEvent) => {
                 console.log("got message", event)
-                this.setState({ messages: [...this.state.messages, JSON.parse(event.data)] })
+                let item = JSON.parse(event.data)
+                this.setState({
+                    data: {
+                        PT_HE: [...this.state.data.PT_HE, item.PT_HE],
+                        PT_Purge: [...this.state.data.PT_Purge, item.PT_Purge],
+                        PT_Pneu: [...this.state.data.PT_Pneu, item.PT_Pneu],
+                        PT_FUEL_PV: [...this.state.data.PT_FUEL_PV, item.PT_FUEL_PV],
+                        PT_LOX_PV: [...this.state.data.PT_LOX_PV, item.PT_LOX_PV],
+                        PT_FUEL_INJ: [...this.state.data.PT_FUEL_INJ, item.PT_FUEL_INJ],
+                        PT_CHAM: [...this.state.data.PT_CHAM, item.PT_CHAM],
+                        TC_FUEL_PV: [...this.state.data.TC_FUEL_PV, item.TC_FUEL_PV],
+                        TC_LOX_PV: [...this.state.data.TC_LOX_PV, item.TC_LOX_PV],
+                        TC_LOX_Valve_Main: [...this.state.data.TC_LOX_Valve_Main, item.TC_LOX_Valve_Main],
+                        RC_LOX_Level: [...this.state.data.RC_LOX_Level, item.RC_LOX_Level],
+                        FT_Thrust: [...this.state.data.FT_Thrust, item.FT_Thrust],
+                    }
+                })
             }
         }
     }
 
     render() {
-        const data_PT_HE: number[] = []
-        const data_PT_Purge: number[] = []
-        const data_PT_Pneu: number[] = []
-        const data_PT_FUEL_PV: number[] = []
-        const data_PT_LOX_PV: number[] = []
-        const data_PT_FUEL_INJ: number[] = []
-        const data_PT_CHAM: number[] = []
-        const data_TC_FUEL_PV: number[] = []
-        const data_TC_LOX_PV: number[] = []
-        const data_TC_LOX_Valve_Main: number[] = []
-        const data_RC_LOX_Level: number[] = []
-        const data_FT_Thrust: number[] = []
 
-        this.state.messages.forEach(val=>{
-            data_PT_HE.push(val.PT_HE)
-            data_PT_Purge.push(val.PT_Purge)
-            data_PT_Pneu.push(val.PT_Pneu)
-            data_PT_FUEL_PV.push(val.PT_FUEL_PV)
-            data_PT_LOX_PV.push(val.PT_LOX_PV)
-            data_PT_FUEL_INJ.push(val.PT_FUEL_INJ)
-            data_PT_CHAM.push(val.PT_CHAM)
-            data_TC_FUEL_PV.push(val.TC_LOX_PV)
-            data_TC_LOX_PV.push(val.TC_LOX_PV)
-            data_TC_LOX_Valve_Main.push(val.TC_LOX_Valve_Main)
-            data_RC_LOX_Level.push(val.RC_LOX_LEVEL)
-            data_FT_Thrust.push(val.FT_THRUST)
-        })
         return (<Grid container>
-            <Grid item xs={6}><Chart data={data_PT_HE} title={"PT_HE"} yaxis={{range:[0, 10], title: "Pressure (PSI)"}}/></Grid>
+            <Grid item xs={6}><Chart data={this.state.data.PT_HE} title={"PT_HE"} yaxis={{ range: [0, 10], title: "Pressure (PSI)" }} /></Grid>
             {/* <Divider orientation="vertical" flexItem></Divider> */}
-            <Grid item xs={6}><Chart data={data_PT_Purge} title={"PT_Purge"} yaxis={{range:[0, 10], title: "Pressure (PSI)"}}/></Grid>
-            <Grid item xs={6}><Chart data={data_PT_Pneu} title={"PT_Pneu"} yaxis={{range:[0, 10], title: "Pressure (PSI)"}}/></Grid>
-            <Grid item xs={6}><Chart data={data_PT_FUEL_PV} title={"PT_Purge"} yaxis={{range:[0, 10], title: "Pressure (PSI)"}}/></Grid>
-            </Grid>)
-            
+            <Grid item xs={6}><Chart data={this.state.data.PT_Purge} title={"PT_Purge"} yaxis={{ range: [0, 10], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={6}><Chart data={this.state.data.PT_Pneu} title={"PT_Pneu"} yaxis={{ range: [0, 10], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={6}><Chart data={this.state.data.PT_FUEL_PV} title={"PT_Purge"} yaxis={{ range: [0, 10], title: "Pressure (PSI)" }} /></Grid>
+        </Grid>)
+
     }
 }
 
