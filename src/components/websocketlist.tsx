0a4d005373d6194cac56dca4a9ff3af48b040e0d
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2021-09-22 23:41:55 -0700

    Added the rest of the sensor message fields

--- src/components/websocketlist.tsx
+++ src/components/websocketlist.tsx
@@ -9,13 +9,17 @@ interface IData {
     PT_Pneu: number[]
     PT_FUEL_PV: number[]
     PT_LOX_PV: number[]
-    PT_FUEL_INJ: number[]
+    //PT_FUEL_INJ: number[]
     PT_CHAM: number[]
     TC_FUEL_PV: number[]
     TC_LOX_PV: number[]
     TC_LOX_Valve_Main: number[]
-    RC_LOX_Level: number[]
+    TC_WATER_In: number[]
+    TC_WATER_Out: number[]
+    TC_CHAM: number[]
+    //RC_LOX_Level: number[]
     FT_Thrust: number[]
+    FL_WATER: number[]
 }
 
 class WebSocketList extends Component<{ ws: WebSocket | null }, { data: IData, timeout: { status: Boolean } }>{
@@ -35,13 +39,17 @@ class WebSocketList extends Component<{ ws: WebSocket | null }, { data: IData, t
                 PT_Pneu: [],
                 PT_FUEL_PV: [],
                 PT_LOX_PV: [],
-                PT_FUEL_INJ: [],
+                //PT_FUEL_INJ: [],
                 PT_CHAM: [],
                 TC_FUEL_PV: [],
                 TC_LOX_PV: [],
                 TC_LOX_Valve_Main: [],
-                RC_LOX_Level: [],
+                TC_WATER_In: [],
+                TC_WATER_Out: [],
+                TC_CHAM: [],
+                //RC_LOX_Level: [],
                 FT_Thrust: [],
+                FL_WATER: []
             }
         }
     }
@@ -119,13 +127,17 @@ class WebSocketList extends Component<{ ws: WebSocket | null }, { data: IData, t
                     PT_Pneu: [],
                     PT_FUEL_PV: [],
                     PT_LOX_PV: [],
-                    PT_FUEL_INJ: [],
+                    //PT_FUEL_INJ: [],
                     PT_CHAM: [],
                     TC_FUEL_PV: [],
                     TC_LOX_PV: [],
                     TC_LOX_Valve_Main: [],
-                    RC_LOX_Level: [],
-                    FT_Thrust: []
+                    TC_WATER_In: [],
+                    TC_WATER_Out: [],
+                    TC_CHAM: [],
+                    //RC_LOX_Level: [],
+                    FT_Thrust: [],
+                    FL_WATER: []
                 }
                 item.message.forEach((val, i) => {
                     console.log(i)
@@ -135,13 +147,17 @@ class WebSocketList extends Component<{ ws: WebSocket | null }, { data: IData, t
                         PT_Pneu: [...data.PT_Pneu, val.PT_Pneu],
                         PT_FUEL_PV: [...data.PT_FUEL_PV, val.PT_FUEL_PV],
                         PT_LOX_PV: [...data.PT_LOX_PV, val.PT_LOX_PV],
-                        PT_FUEL_INJ: [...data.PT_FUEL_INJ, val.PT_FUEL_INJ],
+                        //PT_FUEL_INJ: [...data.PT_FUEL_INJ, val.PT_FUEL_INJ],
                         PT_CHAM: [...data.PT_CHAM, val.PT_CHAM],
                         TC_FUEL_PV: [...data.TC_FUEL_PV, val.TC_FUEL_PV],
                         TC_LOX_PV: [...data.TC_LOX_PV, val.TC_LOX_PV],
                         TC_LOX_Valve_Main: [...data.TC_LOX_Valve_Main, val.TC_LOX_Valve_Main],
-                        RC_LOX_Level: [...data.RC_LOX_Level, val.RC_LOX_Level],
+                        TC_WATER_In: [...data.TC_WATER_In, val.TC_WATER_In],
+                        TC_WATER_Out: [...data.TC_WATER_Out, val.TC_WATER_Out],
+                        TC_CHAM: [...data.TC_CHAM, val.TC_CHAM],
+                        //RC_LOX_Level: [...data.RC_LOX_Level, val.RC_LOX_Level],
                         FT_Thrust: [...data.FT_Thrust, val.FT_Thrust],
+                        FL_WATER: [...data.FL_WATER, val.FL_WATER]
                     }
                 })
                 console.log(data.PT_HE.length)
@@ -152,13 +168,17 @@ class WebSocketList extends Component<{ ws: WebSocket | null }, { data: IData, t
                         PT_Pneu: [...this.state.data.PT_Pneu, ...data.PT_Pneu],
                         PT_FUEL_PV: [...this.state.data.PT_FUEL_PV, ...data.PT_FUEL_PV],
                         PT_LOX_PV: [...this.state.data.PT_LOX_PV, ...data.PT_LOX_PV],
-                        PT_FUEL_INJ: [...this.state.data.PT_FUEL_INJ, ...data.PT_FUEL_INJ],
+                        //PT_FUEL_INJ: [...this.state.data.PT_FUEL_INJ, ...data.PT_FUEL_INJ],
                         PT_CHAM: [...this.state.data.PT_CHAM, ...data.PT_CHAM],
                         TC_FUEL_PV: [...this.state.data.TC_FUEL_PV, ...data.TC_FUEL_PV],
                         TC_LOX_PV: [...this.state.data.TC_LOX_PV, ...data.TC_LOX_PV],
                         TC_LOX_Valve_Main: [...this.state.data.TC_LOX_Valve_Main, ...data.TC_LOX_Valve_Main],
-                        RC_LOX_Level: [...this.state.data.RC_LOX_Level, ...data.RC_LOX_Level],
+                        TC_WATER_In: [...this.state.data.TC_WATER_In, ...data.TC_WATER_In],
+                        TC_WATER_Out: [...this.state.data.TC_WATER_Out, ...data.TC_WATER_Out],
+                        TC_CHAM: [...this.state.data.TC_CHAM, ...data.TC_CHAM],
+                        //RC_LOX_Level: [...this.state.data.RC_LOX_Level, ...data.RC_LOX_Level],
                         FT_Thrust: [...this.state.data.FT_Thrust, ...data.FT_Thrust],
+                        FL_WATER: [...this.state.data.FL_WATER, ...data.FL_WATER]
                     }
                 })
             }
@@ -168,18 +188,22 @@ class WebSocketList extends Component<{ ws: WebSocket | null }, { data: IData, t
     render() {
 
         return (<Grid container>
-            <Grid item xs={6}><Chart data={this.state.data.PT_HE} title={"PT_HE"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.PT_Purge} title={"PT_Purge"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.PT_Pneu} title={"PT_Pneu"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.PT_FUEL_PV} title={"PT_FUEL_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.PT_LOX_PV} title={"PT_LOX_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.PT_FUEL_INJ} title={"PT_FUEL_INJ"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.PT_CHAM} title={"PT_CHAM"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.TC_FUEL_PV} title={"TC_FUEL_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.TC_LOX_PV} title={"TC_LOX_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.TC_LOX_Valve_Main} title={"TC_LOX_Valve_Main"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.RC_LOX_Level} title={"RC_LOX_Level"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
-            <Grid item xs={6}><Chart data={this.state.data.FT_Thrust} title={"FT_Thrust"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.PT_HE} title={"PT_HE"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.PT_Purge} title={"PT_Purge"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.PT_Pneu} title={"PT_Pneu"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.PT_FUEL_PV} title={"PT_FUEL_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.PT_LOX_PV} title={"PT_LOX_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            {/*<Grid item xs={3}><Chart data={this.state.data.PT_FUEL_INJ} title={"PT_FUEL_INJ"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>*/}
+            <Grid item xs={3}><Chart data={this.state.data.PT_CHAM} title={"PT_CHAM"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.TC_FUEL_PV} title={"TC_FUEL_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.TC_LOX_PV} title={"TC_LOX_PV"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.TC_LOX_Valve_Main} title={"TC_LOX_Valve_Main"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.TC_WATER_In} title={"TC_WATER_In"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.TC_WATER_Out} title={"TC_WATER_Out"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.TC_CHAM} title={"TC_CHAM"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            {/*<Grid item xs={3}><Chart data={this.state.data.RC_LOX_Level} title={"RC_LOX_Level"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>*/}
+            <Grid item xs={3}><Chart data={this.state.data.FT_Thrust} title={"FT_Thrust"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
+            <Grid item xs={3}><Chart data={this.state.data.FL_WATER} title={"FL_WATER"} yaxis={{ range: [0, 500], title: "Pressure (PSI)" }} /></Grid>
         </Grid>)
 
     }
